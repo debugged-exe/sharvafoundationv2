466431bbc722acc19138af90b1843e2461d4d206
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-12-08 02:28:00 +0530

    Added Testimonials,AboutUs

--- src/Container/App.js
+++ src/Container/App.js
@@ -6,12 +6,16 @@ import './App.css';
 import Counter from '../Component/CounterUp/counter.jsx';
 import '../Component/CounterUp/counterUp.css'
 
+import Testimonial from '../Component/Testimonial/Testimonial';
+import AboutUs from '../Component/AboutUs/AboutUs.js';
 class App extends Component {
   render() {
     return(
       <div className="App">
         <Navigation src = {Logo}/>
+        <AboutUs/>
         <Counter />
+        <Testimonial/>
       </div>
     );
   }
