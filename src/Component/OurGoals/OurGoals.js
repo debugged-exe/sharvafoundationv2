a62da560919f616a624dfc0142bcb89d7273f359
tejasjokin <61027009+tejasjokin@users.noreply.github.com>
2020-12-15 23:50:17 +0530

    Frontend fixes

--- src/Component/OurGoals/OurGoals.js
+++ src/Component/OurGoals/OurGoals.js
@@ -9,18 +9,15 @@ const OurGoals = ({goal}) =>
 
     let btnStyle;
 
-    const [hover, toggleHover] = useState(false);
+    const [hover1, toggleHover1] = useState(false);
+    const [hover2, toggleHover2] = useState(false);
 
-    const toggleHandler = () => {
-        toggleHover(!hover)
-        if(hover)
-        {
-            btnStyle = {color: 'white', border:'2px solid #e88f0a'}
-        }
-        else
-        {
-            btnStyle = {color: 'black', border:'2px solid #e88f0a'}
-        }
+    const toggleHandler1 = () => {
+        toggleHover1(!hover1)
+    }
+
+    const toggleHandler2 = () => {
+        toggleHover2(!hover2)
     }
 
 	const {total, complete} = goal;
@@ -39,8 +36,8 @@ const OurGoals = ({goal}) =>
         		<div style = {{width: `${width}%`}} className="filled br3 tc pa1 f5 white fw5">{`₹ ${complete} out of ₹ ${total}`}</div>
             </div>
             <div data-aos='fade-up' className='w-60 mt4 flex justify-center'>
-            	<div style={btnStyle} onMouseEnter = {() => toggleHandler()} onMouseLeave = {() => toggleHandler()} className = "mh2 br3 ba btn ph4 pv3 grow">MAKE DONATION</div>
-            	<div style={btnStyle} className = "mh2 br3 ba btn ph4 pv3 grow">BECOME A VOLUNTEER</div>
+            	<div style={hover1?{color: 'white', fontWeight: 'bold', border:'1.5px solid #e88f0a'}:{color: '#e88f0a', fontWeight: 'bold', border:'1.5px solid #e88f0a'}} onMouseEnter = {() => toggleHandler1()} onMouseLeave = {() => toggleHandler1()} className = "mh2 br3 ba btn ph4 pv3 grow">MAKE DONATION</div>
+            	<div style={hover2?{color: 'white', fontWeight: 'bold', border:'1.5px solid #e88f0a'}:{color: '#e88f0a', fontWeight: 'bold', border:'1.5px solid #e88f0a'}} onMouseEnter = {() => toggleHandler2()} onMouseLeave = {() => toggleHandler2()} className = "mh2 br3 ba btn ph4 pv3 grow">BECOME A VOLUNTEER</div>
             </div>
         </div>
     );
