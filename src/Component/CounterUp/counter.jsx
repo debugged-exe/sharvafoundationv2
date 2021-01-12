afdadcfb4b14b8876a261cef7d69598ebf9d7462
tanmayjagtap <tanmayjagtap27@gmail.com>
2021-01-12 19:12:08 +0530

    Removed about us

--- src/Component/CounterUp/counter.jsx
+++ src/Component/CounterUp/counter.jsx
@@ -1,41 +1,32 @@
 import React from "react";
-
-/*import counterUp from "./counterUp";*/
-
-import CounterUp from "react-countup";
+import { TiGroup } from "react-icons/ti";
+import { GiForkKnifeSpoon } from "react-icons/gi";
+import { RiPlantFill,RiWomenFill } from "react-icons/ri";
 
 function Counter() {
   return (
-    <section id="counter">
-      <div className="box-1">
-        <p className="fa fa-users"></p>
-        <div className="counter" data-count="110">
-          <CounterUp end={110} duration={5} />
-        </div>
+    <div className='flex flex-wrap justify-around items-center f3 pointer'>
+      <div style={{width:'300px',background:'#e88f0a'}} className='flex flex-wrap justify-center items-center flex-column ba br3 pa3 ma2 grow shadow-4'>
+        <TiGroup size='2.5rem' />
         <p>Volunteers</p>
+        <p>150</p>
       </div>
-      <div className="box-2">
-        <p className="fa fa-pagelines"></p>
-        <div className="counter" data-count="41">
-          <CounterUp end={41} duration={5} />
-        </div>
-        <p>Tree Planted</p>
+      <div style={{width:'300px', background:'#e1a448'}} className='flex flex-wrap justify-center items-center flex-column ba br3 pa3 ma2 grow shadow-4'>
+        <RiPlantFill size='2.5rem' />
+        <p>Trees Planted</p>
+        <p>150</p>
       </div>
-      <div className="box-3">
-        <p className="fa fa-cutlery"></p>
-        <div className="counter" data-count="174">
-          <CounterUp end={174} duration={5} />
-        </div>
+      <div style={{width:'300px', background:'#efbc70'}} className='flex flex-wrap justify-center items-center flex-column ba br3 pa3 ma2 grow shadow-4'>
+        <GiForkKnifeSpoon size='2.5rem' />
         <p>Food Kits</p>
+        <p>150</p>
       </div>
-      <div className="box-4">
-        <p className="fa fa-plus-square"></p>
-        <div className="counter" data-count="15">
-          <CounterUp end={15} duration={2} />
-        </div>
-        <p>Sanitary Pads</p>
+      <div style={{width:'300px', background:'#efbc99'}} className='flex flex-wrap justify-center items-center flex-column ba br3 pa3 ma2 grow shadow-4'>
+        <RiWomenFill size='2.5rem' />
+        <p>Sanitary Napkins</p>
+        <p>150</p>
       </div>
-    </section>
+    </div>
   );
 }
 
--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './Footer.css';
 import { FaFacebook,FaInstagram,FaWhatsapp} from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
+import { AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
     return (
         <footer className='bg-black white pv2 bottom'>
         <div className="ml3 mr3 flex justify-between pointer items-center flex-wrap h-15">
-            <div className='f4'><a>Copyrights</a></div>
-            <div className='f4'><a>Privacy Policy</a></div>
-            <div className="flex f3 justify-around">
-                <a className='pa2 link white dim ph3'><FaFacebook color='white' /></a>
-                <a className='pa2 link white dim ph3'><FaInstagram color='white'/></a>
-                <a className='pa2 link white dim ph3'><FaWhatsapp color='white'/></a>
-                <a className='pa2 link white dim ph3'><SiGmail color='white'/></a>
+            <div className='f5'>&copy; Copyrights 2020 | sharvafoundation.org | All rights reserved</div>
+            <div className='f5'>Privacy Policy</div>
+            <div className="flex f4 justify-around">
+                <a style={{outline:'none'}} href='https://www.facebook.com/Sharva-Foundation-571908020152850/?modal=admin_todo_tour' className='pa2 link white dim'><FaFacebook color='white' /></a>
+                <a style={{outline:'none'}} href='https://instagram.com/sharvafoundation?igshid=yubeg3h96wn4' className='pa2 link white dim'><FaInstagram color='white'/></a>
+                <a style={{outline:'none'}} href='https://wa.me/917517502268' className='pa2 link white dim'><FaWhatsapp color='white'/></a>
+                <a style={{outline:'none'}} href='https://twitter.com/Sharvacharity' className='pa2 link white dim'><AiOutlineTwitter color='white'/></a>
+                <a style={{outline:'none'}} href="mailto:foundationsharva@gmail.com?subject=Joining a good cause" className='pa2 link white dim'><SiGmail color='white'/></a>
             </div>
         </div>
         </footer>
